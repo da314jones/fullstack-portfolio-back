a630a2ca3e10aa914321d821dd2b2f5d4e347fd5
D Jones <djones@Ds-MacBook-Air.mynetworksettings.com>
2023-11-16 14:37:32 -0500

    server table situated no values

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS ?_dev;
+CREATE DATABASE ?_dev;
+
+/c ?_dev;
+CREATE TABLE ? (
+    id SERIAL PRIMARY KEY,
+    "remaining table values upon app idea approval"
+);
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+/c ?_dev;
+
+INSERT INTO ? (
+    "value headers from table"
+) VALUES "actual generated data"
