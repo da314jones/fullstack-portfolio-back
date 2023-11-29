e784bda8d6026eb48ef8684cf6a24dea7cbce804
D Jones <djones@Ds-MacBook-Air.mynetworksettings.com>
2023-11-29 06:34:41 -0500

    presentation commit

--- db/schema.sql
+++ db/schema.sql
@@ -15,12 +15,13 @@ CREATE TABLE entries (
     id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     rating_before INTEGER CHECK (rating_before >= 1 AND rating_before <= 5),
-    adjective_before VARCHAR(255), -- Assuming you store the mood adjective directly
+    adjective_before VARCHAR(255),
     description TEXT NOT NULL,
+    is_service_related BOOLEAN,
     service_related_notes TEXT,
     activity VARCHAR(255),
     custom_activity TEXT,
-    adjective_after VARCHAR(255), -- Assuming you store the mood adjective directly
+    adjective_after VARCHAR(255), 
     rating_after INTEGER CHECK (rating_after >= 1 AND rating_after <= 5),
     updated_at DATE DEFAULT CURRENT_DATE
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,19 +1,18 @@
 \c journal_dev;
-
-INSERT INTO entries (date, rating_before, adjective_before, description, service_related_notes, activity, custom_activity, adjective_after, rating_after) VALUES
-('2023-03-23', 1, 'Stressed', 'Health issues caused anxiety.', 'Health worries', NULL, NULL, 'Stressed', 1),
-('2023-03-24', 3, 'Energetic', 'Joined a dance class.', NULL, 'Dancing', 4, 'Energetic', 5),
-('2023-03-25', 4, 'Relaxed', 'Listened to my favorite music.', NULL, 'Music listening', 3, 'Relaxed', 4),
-('2023-03-26', 2, 'Anxious', 'Nervous about an upcoming interview.', 'Job interview anxiety', NULL, NULL, 'Anxious', 2),
-('2023-03-27', 5, 'Happy', 'Cooked a special meal for friends.', NULL, 'Cooking', 4, 'Happy', 5),
-('2023-03-28', 3, 'Calm', 'Spent the day journaling my thoughts.', NULL, 'Journaling', 3, 'Calm', 3),
-('2023-03-29', 4, 'Optimistic', 'Went hiking and enjoyed the scenery.', NULL, 'Hiking', 4, 'Optimistic', 4),
-('2023-03-30', 2, 'Irritated', 'Difficult day at work.', 'Work stress', NULL, NULL, 'Irritated', 2),
-('2023-03-31', 5, 'Excited', 'Started a new hobby, feeling enthusiastic.', NULL, 'Photography', 4, 'Excited', 5),
-('2023-04-01', 1, 'Sad', 'Reflecting on some past events.', 'Personal reflection', NULL, NULL, 'Sad', 1),
-('2023-04-02', 3, 'Energetic', 'Went cycling, felt energized.', NULL, 'Cycling', 4, 'Energetic', 3),
-('2023-04-03', 4, 'Relaxed', 'Enjoyed a day of bird watching.', NULL, 'Bird Watching', 3, 'Relaxed', 4),
-('2023-04-04', 2, 'Anxious', 'Worried about a family member.', 'Family concern', NULL, NULL, 'Anxious', 2),
-('2023-04-05', 5, 'Happy', 'Had a fun day learning to cook new dishes.', NULL, 'Cooking Class', 4, 'Happy', 5),
-('2023-04-06', 3, 'Calm', 'Volunteered at a local event, felt fulfilled.', NULL, 'Volunteering', 5, 'Calm', 3),
-('2023-04-07', 4, 'Optimistic', 'Engaged in art therapy, felt positive.', NULL, 'Art Therapy', 4, 'Optimistic', 4);
+INSERT INTO entries (date, rating_before, adjective_before, description, is_service_related, service_related_notes, activity, custom_activity, adjective_after, rating_after) VALUES
+('2023-03-23', 1, 'Stressed', 'Health issues caused anxiety.', false, 'Health worries', NULL, NULL, 'Stressed', 1),
+('2023-03-24', 3, 'Energetic', 'Joined a dance class.', false, NULL, 'Dancing', 4, 'Energetic', 5),
+('2023-03-25', 4, 'Relaxed', 'Listened to my favorite music.', false, NULL, 'Music listening', 3, 'Relaxed', 4),
+('2023-03-26', 2, 'Anxious', 'Nervous about an upcoming interview.', false, 'Job interview anxiety', NULL, NULL, 'Anxious', 2),
+('2023-03-27', 5, 'Happy', 'Cooked a special meal for friends.', false, NULL, 'Cooking', 4, 'Happy', 5),
+('2023-03-28', 3, 'Calm', 'Spent the day journaling my thoughts.', false, NULL, 'Journaling', 3, 'Calm', 3),
+('2023-03-29', 4, 'Optimistic', 'Went hiking and enjoyed the scenery.', false, NULL, 'Hiking', 4, 'Optimistic', 4),
+('2023-03-30', 2, 'Irritated', 'Difficult day at work.', false, 'Work stress', NULL, NULL, 'Irritated', 2),
+('2023-03-31', 5, 'Excited', 'Started a new hobby, feeling enthusiastic.', false, NULL, 'Photography', 4, 'Excited', 5),
+('2023-04-01', 1, 'Sad', 'Reflecting on some past events.', false, 'Personal reflection', NULL, NULL, 'Sad', 1),
+('2023-04-02', 3, 'Energetic', 'Went cycling, felt energized.', false, NULL, 'Cycling', 4, 'Energetic', 3),
+('2023-04-03', 4, 'Relaxed', 'Enjoyed a day of bird watching.', false, NULL, 'Bird Watching', 3, 'Relaxed', 4),
+('2023-04-04', 2, 'Anxious', 'Worried about a family member.', false, 'Family concern', NULL, NULL, 'Anxious', 2),
+('2023-04-05', 5, 'Happy', 'Had a fun day learning to cook new dishes.', false, NULL, 'Cooking Class', 4, 'Happy', 5),
+('2023-04-06', 3, 'Calm', 'Volunteered at a local event, felt fulfilled.', false, NULL, 'Volunteering', 5, 'Calm', 3),
+('2023-04-07', 4, 'Optimistic', 'Engaged in art therapy, felt positive.', false, NULL, 'Art Therapy', 4, 'Optimistic', 4);
