53ed14936d4578e830f5ae32c3196fa0729becf0
DJones <dwaynejones@pursuit.com>
2023-12-06 13:51:31 -0500

    for deployment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -3,7 +3,6 @@ require("dotenv").config()
 
 const cn = {
 
-    databaseURL: process.env.PG_URL,
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
